15926f75cdc5e1703d8661f1c92aab34abe929e9
Jeremy Carbaugh <jcarbaugh@gmail.com>
2012-03-22 17:24:05 -0400

    conditionally load chartbeat

--- src/movingup/static/js/script.js
+++ src/movingup/static/js/script.js
@@ -1,3 +1,20 @@
+var jsloader = function(d, s, id, url) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s);
+	js.id = id;
+	js.src = url;
+	fjs.parentNode.insertBefore(js, fjs);
+};
+
+var probablyNotMobile = $(document).width() >= 960;
+
 $(function(){
+	
 	$("input").placehold();
+
+	if (probablyNotMobile) {
+		jsloader(document, 'script', 'chartbeat-js', 'http://static.chartbeat.com/js/chartbeat.js');
+	}
+
 });
